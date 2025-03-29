28b91cfc873db40d875b9417679150d9c0cd9815
Tran Duong <duong.tran@wisewires.com>
2025-03-29 10:57:09 +0700

    change last name before first name

--- webapp/channels/src/packages/mattermost-redux/src/utils/user_utils.ts
+++ webapp/channels/src/packages/mattermost-redux/src/utils/user_utils.ts
@@ -12,7 +12,7 @@ import {General, Preferences} from '../constants';
 
 export function getFullName(user: UserProfile): string {
     if (user.first_name && user.last_name) {
-        return user.first_name + ' ' + user.last_name;
+        return user.last_name + ' ' + user.first_name;
     } else if (user.first_name) {
         return user.first_name;
     } else if (user.last_name) {
